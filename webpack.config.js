2956c00b85d18b0e61dcbfc474e20c923431319f
Danny Yoo <dansteryoo@gmail.com>
2020-02-05 08:27:53 -0500

    inital setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// webpack.config.js
+var path = require('path');
+
+module.exports = {
+    entry: './frontend/index.jsx',
+    output: {
+        filename: 'app/assets/javascripts',
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+};
